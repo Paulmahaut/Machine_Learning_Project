7ddcadf0bb69d4e85d69b0ad507efd2f054a15f7
AlexisLS05 <151126331+AlexisLS05@users.noreply.github.com>
2025-12-03 14:06:56 +0530

    Essai
    j'essaie d'implémenter gru mais ce n'est vraiment pas bon.
    Pour test il faut run xgboostEval2 puis gru_prep et gru_train.

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 import yfinance as yf
 import pandas as pd
 
-# Télécharger TotalEnergies depuis Yahoo Finance
-df = yf.download("TTE.PA", start="2005-01-01", end="2025-01-01", progress=False)
+# Télécharger Tesla depuis Yahoo Finance
+df = yf.download("TSLA", start="2005-01-01", end="2025-01-01", progress=False, auto_adjust=True)
 
-df.to_excel("TotalEnergies_2005_2025.xlsx")
+df.to_excel("Tesla_2005_2025.xlsx")
 
 print(df.head())
 print(df.shape)
